5622e92c8acd98888f84bf810308ad4a1702f40f
unknown <pooja.s@carbonyte.io>
2023-01-16 09:35:36 +0000

    changed

--- App.js
+++ App.js
@@ -90,7 +90,7 @@ import CarbonCart from "./app/screens/CarbonCart";
 const Stack = createNativeStackNavigator();
 const StackNavigator = () => (
   
-  <Stack.Navigator initialRouteName="MyCards">
+  <Stack.Navigator initialRouteName="Onboarding3">
     <Stack.Screen  name="SplashAnimation" component={SplashAnimation}/>
 
     <Stack.Screen  name="Onboarding1" component={Onboarding1}/>
