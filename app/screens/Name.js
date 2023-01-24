7f7040ca2215f8d91f88cff3d3b67d557ba4f878
Jack Huang <jack.h@carbonyte.io>
2023-01-24 13:15:22 +0000

    Updated Name.js

--- app/screens/Name.js
+++ app/screens/Name.js
@@ -218,7 +218,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingLeft: "10%",
     paddingTop: GlobalStyles.Padding.padding_7xs,
-    paddingRight: GlobalStyles.Padding.padding_8xs,
+    paddingRight: "10%",
     width: "100%",
     backgroundColor: GlobalStyles.Color.grey,
   },
