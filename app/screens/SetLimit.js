4d76ef33aa189879acb7b485bf0a8b75b8fd2a0e
Naga Guda <naga.g@carbonyte.io>
2023-01-25 15:26:20 +0530

    MyCards API

--- app/screens/SetLimit.js
+++ app/screens/SetLimit.js
@@ -189,7 +189,7 @@ const styles = StyleSheet.create({
   hello5: {
     top: "38.33%",
     left: "50.00%",
-    marginLeft:-20,
+    marginLeft:-40,
     fontSize: GlobalStyles.FontSize.size_lg,
     textTransform: "uppercase",
     color: GlobalStyles.Color.black,
