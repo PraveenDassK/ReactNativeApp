5d65d220aaca33c695a3158517e0ececcd845e39
Jack Huang <jack.h@carbonyte.io>
2023-01-26 11:22:30 +0000

    Updated Settings.js

--- app/screens/Settings.js
+++ app/screens/Settings.js
@@ -12,9 +12,9 @@ const Settings = ({navigation}) => {
   const [plan, setPlan] = useState(null)
   const [balance, setBal] = useState(null)
   const [currency, setCurrency] = useState(null)
+  const [initials, setInitals] = useState(null)
   const authContext = useContext(AuthContext)
 
-  console.log(account)
   useEffect(() => {
     loadData()
   },[])
@@ -31,6 +31,15 @@ const Settings = ({navigation}) => {
     setSortCode(accountdata.identifiers[0].sortCode)
     setAccNum(accountdata.identifiers[0].accountNumber)
     setCurrency(data.currency ? data.currency : "GBP")
+
+    let name = accountdata.customerName
+    let names = name.split(' '), initialsHold = names[0].substring(0, 1).toUpperCase();
+    
+    if (names.length > 1) {
+      initialsHold += names[names.length - 1].substring(0, 1).toUpperCase();
+    }
+    console.log(initialsHold)
+    setInitals(initialsHold)
   }
 
   const copyAccount = () => {
@@ -60,15 +69,13 @@ const Settings = ({navigation}) => {
         />
         <Text style={styles.hello}>{fullname}</Text>
         <Text style={styles.hello1}>{plan}</Text>
-        <Image
+        <Text
           style={[
             styles.groupChild,
             styles.groupLayout,
             styles.groupChildLayout,
           ]}
-          resizeMode="cover"
-          source={require("../assets/ellipse-3158.png")}
-        />
+        >{initials}</Text>
         <Pressable
           style={styles.helloParent}
           onPress = {() =>navigation.navigate("Login")} 
