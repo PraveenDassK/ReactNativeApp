446c6c61cf5abfe84f3258a7e9d63681e17c37ee
ryan.s <2jrp2oauddiqqrrmzgvx3qx5j7hmlrohnjl3aoqfvv3vuimhg35a>
2023-02-16 16:50:08 +0000

    Spending limit 50% styled

--- app/screens/SpendingLimit.js
+++ app/screens/SpendingLimit.js
@@ -1,17 +1,17 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Text, StyleSheet, View, Image, Pressable, Switch, Button } from "react-native";
+import { Text, StyleSheet, View, Image, Pressable, Switch, Button, ScrollView } from "react-native";
 import GlobalStyles from "../../GlobalStyles";
 import AuthContext from "../auth/context";
 import Screen from "../components/Screen";
-import api from "../api/api_list"
-import apiCall from "../api/api"
-import { horizontalScale, verticalScale, moderateScale } from "../config/scaling"
+import api from "../api/api_list";
+import apiCall from "../api/api";
+import { horizontalScale, verticalScale, moderateScale } from "../config/scaling";
 import { useFocusEffect } from "@react-navigation/native";
 
-const SpendingLimit = ({navigation,route}) => {
+const SpendingLimit = ({ navigation, route }) => {
   const [isEnabled, setIsEnabled] = useState(false);
-  const authContext = useContext(AuthContext)
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const authContext = useContext(AuthContext);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
   const [monthLim, setMonLim] = useState(0);
   const [spend, setSpend] = useState(0);
@@ -19,9 +19,9 @@ const SpendingLimit = ({navigation,route}) => {
 
   //Calls the API once during load
   useEffect(() => {
-    loadData()
-  },[])
-  
+    loadData();
+  }, []);
+
   //Calls the API once during load
   useFocusEffect(() => {
     const unsubscribe = navigation.addListener("focus", () => {
@@ -33,321 +33,164 @@ const SpendingLimit = ({navigation,route}) => {
   const loadData = async () => {
     //Gets the data from the api
     const response = await api.GetLimit(authContext.accountID);
-    //Then isolate the useful data
-    const data = response.data.details
-    console.log(data)
+    //Then isolate the useful dataa
+    const data = response.data.details;
+    console.log(data);
 
     //Gets the monthly spend
-    const transactionRequest = await apiCall.GetAllTransactionsThisMonth()
-    let spendTotal = transactionRequest.total
+    const transactionRequest = await apiCall.GetAllTransactionsThisMonth();
+    let spendTotal = transactionRequest.total;
 
     //If there is a limit
     //50 is the amount spent
-    if(data.monthlyAmount > 0){
-      const percentamount = Math.floor((spendTotal/data.monthlyAmount)*100) + "%"
-      setPercent(percentamount)
-      setIsEnabled(true)
-    }else{
-      setPercent("0%")
+    if (data.monthlyAmount > 0) {
+      const percentamount = Math.floor((spendTotal / data.monthlyAmount) * 100) + "%";
+      setPercent(percentamount);
+      setIsEnabled(true);
+    } else {
+      setPercent("0%");
     }
 
-    setMonLim(data.monthlyAmount)
+    setMonLim(data.monthlyAmount);
     setSpend(spendTotal);
-    console.log(data.monthlyAmount)
-  }
+    console.log(data.monthlyAmount);
+  };
 
   /**
    * @todo set spending limit to 0 on trigger
    */
   const spendingToggle = () => {
-    if(isEnabled){
-      setIsEnabled(false)
-      setPercent("0%")
-      setMonLim(0)
-    }else{
-      setIsEnabled(true)
-      navigation.navigate("SetLimit")
+    if (isEnabled) {
+      setIsEnabled(false);
+      setPercent("0%");
+      setMonLim(0);
+    } else {
+      setIsEnabled(true);
+      navigation.navigate("SetLimit");
+      console.log(setIsEnabled);
     }
-  }
+  };
 
   return (
-    <Screen>
-    <View style={styles.spendingLimit}>
-     
-      <View style={styles.helloParent}>
-        <Text style={[styles.hello, styles.limitFlexBox]}>Spending limit</Text>
-        <View
-          style={[
-            styles.rectangleParent,
-            styles.groupChildPosition,
-            styles.groupChildPosition1,
-          ]}
-        >
-          <View
-            style={[
-              styles.groupChild,
-              styles.groupChildPosition,
-              styles.groupChildPosition1,
-            ]}
-          />
-          <View
-            style={[
-              styles.cardSpendingLimitTheSpendParent,
-              styles.rectangleGroupPosition,
-            ]}
-          >
-            <Text
-              style={[styles.cardSpendingLimitTheSpend, styles.limitFlexBox]}
-            >
-              <Text style={styles.cardSpendingLimit}>
-                <Text style={styles.cardSpendingLimit1}>
-                  Card spending limit
-                </Text>
-              </Text>
-              <Text style={styles.cardSpendingLimit}>
-                <Text
-                  style={[styles.theSpend, styles.theColor]}
-                >{`\n The spend & withdrawal cap`}</Text>
-              </Text>
-            </Text>
-            <Image
-              style={[styles.meter1Icon, styles.groupChildPosition]}
-              resizeMode="cover"
-              source={require("../assets/meter-1.png")}
-            />
-          </View>
-          <Switch  
-            style={[styles.rectangleGroup, styles.rectangleGroupPosition]}
-            trackColor={{false: GlobalStyles.Color.gray_600, true:GlobalStyles.Color.blue_100}}
-            thumbColor={isEnabled ?'#f4f3f4' : '#f4f3f4'}
-            onValueChange={toggleSwitch}
-            value={isEnabled}
-            />
-          <Pressable
-            style={[styles.rectangleGroup, styles.rectangleGroupPosition]}
-            onPress={() => spendingToggle()}
-            title="Set Limit"
-          />
-
-          <Text style={[styles.amountText]}>
-            Amount spent this month : £{spend}
-          </Text>
-          <Text style={[styles.amountText]}>
-            Limit this Month : £{monthLim}
-          </Text>
-
-          <View style={[styles.amountContainer]}>
-
-            <View style={[styles.amountScale]} width = {percent}>
-
-            </View>
-          </View>
-
-        </View>
-        
-        <Text
-          style={[
-            styles.theLimitDeterminesTheAmoun,
-            styles.theColor,
-            styles.limitFlexBox,
-          ]}
-        >
-          {spend}
-          The limit determines the amount that can be spent or withdrawn using
-          this card per month
-        </Text>
-
+  <ScrollView>
+    <View style={styles.mainContainer}>
+      <View style={styles.titleTextRow}>
+        <Text style={styles.titleText}>Spending Limit</Text>
       </View>
+
+      <View style={styles.subTextRow}>
+        <Text style={styles.subText}>The limit determines the amount that can be spent or withdrawn using this card per month</Text>
+      </View>
+
+      <View
+        style={{
+          width: "90%",
+          marginLeft: "5%",
+          backgroundColor: "white",
+          borderRadius: 15,
+          height: 500,
+          marginTop: "5%",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            width: "100%",
+            justifyContent: "space-between",
+            alignItems: "center",
+            height: 50,
+            padding: "5%",
+          }}
+        >
+          <Image style={{ flex: 2, height: 35, width: 35 }} resizeMode="contain" source={require("../assets/meter-1.png")} />
+          <View style={{ flex: 6, marginLeft: "5%" }}>
+            <Text style={{ fontWeight: "700", fontSize: 16 }}>Card Spending limit</Text>
+            <Text style={{ fontSize: 10 }}>The spend & withdrawal cap</Text>
+          </View>
+          <Pressable style={{ height: 50, width: "auto" }} onPress={() => console.log("?")} title="Set Limit">
+            <Switch
+              style={{ flex: 2 }}
+              trackColor={{
+                false: GlobalStyles.Color.gray_600,
+                true: GlobalStyles.Color.blue_100,
+              }}
+              thumbColor={isEnabled ? "#f4f3f4" : "#f4f3f4"}
+              onValueChange={spendingToggle}
+              value={isEnabled}
+            />
+          </Pressable>
+        </View>
+        <View style={{ width: "100%", marginTop: "20%", justifyContent: "center", alignItems: "center" }}>
+          <Text style={{ fontSize: 25, fontWeight: "700" }}>£{spend}</Text>
+          <Text style={{ fontSize: 14, marginTop: "1%" }}>Spent this month</Text>
+          <Text style={{ fontSize: 25, marginTop: "2.5%", fontWeight: "700" }}>£{monthLim}</Text>
+          <Text style={{ fontSize: 14, marginTop: "1%" }}>Current spend limit</Text>
+          {monthLim - spend >= 0 ? (
+            <View style={{ maxWidth: "90%", marginTop: "2.5%", height: 35, borderRadius: 15 }} width={percent} backgroundColor="#F6F5F8">
+              <Text style={styles.barText}>test</Text>
+            </View>
+          ) : (
+            <View style={{ maxWidth: "90%", marginTop: "2.5%", height: 35, borderRadius: 15 }} width={percent} backgroundColor="red">
+              <Text style={styles.barText}>£{monthLim - spend}</Text>
+            </View>
+          )}
+
+          {monthLim - spend >= 0 ? <Text style={{ fontSize: 25, color: "blue", fontWeight: "700" }}> £{monthLim - spend}</Text> : <Text style={{ fontSize: 25, marginTop: "2.5%", color: "red", fontWeight: "700" }}> £{monthLim - spend}</Text>}
+          <Text style={{ fontSize: 14, marginTop: "1%" }}>Spendable funds left</Text>
+        </View>
+
+        <View style={{ width: "100%", flex: 1, justifyContent: "flex-end", alignItems: "center", marginBottom: "5%" }}>
+          <Image style={{ height: 35, width: 35 }} resizeMode="contain" source={require("../assets/card.png")} />
+          {isEnabled ? <Text style={{ fontSize: 10, marginTop: "2.5%", fontWeight: "700" }}>Limit is toggled on</Text> : <Text style={{ fontSize: 10, marginTop: "2.5%", fontWeight: "700" }}>Limit is toggled off</Text>}
+        </View>
+      </View>
+      <View style={{width: "100%", height: 35}}/>
     </View>
-    </Screen>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  amountText:{
-    width:"100%",
-    textAlign:"center",
-    top:"30%",
-  },
-  amountContainer:{
-    width:"90%",
-    left: "5%",
-    height:"10%",
-    backgroundColor:"grey",
-    top:"40%",
-    borderRadius: 20
-
-  },
-  amountScale:{
-    height:"100%",
-    backgroundColor:"blue",
-    borderRadius: 20
+  mainContainer: {
+    backgroundColor: GlobalStyles.DivContainer.backgroundColor,
+    height: GlobalStyles.DivContainer.height,
+    width: "100%",
+    flex: GlobalStyles.DivContainer.flex,
   },
 
-  limitFlexBox: {
-    textAlign: "left",
-    position: "absolute",
+  titleTextRow: {
+    marginTop: GlobalStyles.Title.marginTop,
+    width: GlobalStyles.DivContainer.width,
+    marginLeft: GlobalStyles.DivContainer.marginLeft,
   },
-  groupChildPosition: {
-    left: 0,
-    position: "absolute",
+
+  titleText: {
+    fontSize: GlobalStyles.Title.fontSize,
+    fontWeight: GlobalStyles.Title.fontWeight,
   },
-  groupChildPosition1: {
-    right: 0,
-    left: 0,
+
+  subText: {
+    fontSize: GlobalStyles.RowText.fontSize,
+    fontWeight: GlobalStyles.RowText.fontWeight,
   },
-  rectangleGroupPosition: {
-    right: 22,
-    position: "absolute",
+
+  subTextRow: {
+    marginTop: GlobalStyles.RowText.marginTop,
+    width: GlobalStyles.DivContainer.width,
+    marginLeft: GlobalStyles.DivContainer.marginLeft,
+    fontColor: GlobalStyles.RowText.fontColor,
   },
-  theColor: {
-    color: GlobalStyles.Color.gray_800,
-    // fontFamily: GlobalStyles.FontFamily.helvetica,
-  },
-  groupLayout: {
-    overflow: "hidden",
-    maxWidth: "100%",
-    position: "absolute",
-  },
-  helloPosition: {
-    color: GlobalStyles.Color.gray_700,
-    left: "50%",
-    // fontFamily: GlobalStyles.FontFamily.helvetica,
-    textAlign: "center",
-    position: "absolute",
-  },
-  hello: {
-    left: 2,
-    fontSize: GlobalStyles.FontSize.size_8xl,
-    color: GlobalStyles.Color.indigo_100,
-    fontWeight: "700",
-    top: 0,
-  },
-  groupChild: {
-    marginTop: -181,
-    borderRadius: GlobalStyles.Border.br_5xl,
-    backgroundColor: GlobalStyles.Color.white,
-    height: 362,
-    top: "50%",
-  },
-  cardSpendingLimit1: {
-    color: GlobalStyles.Color.gray_1400,
-    // fontFamily: GlobalStyles.FontFamily.helvetica,
-    fontSize: GlobalStyles.FontSize.size_base,
-    fontWeight: "700",
-  },
-  cardSpendingLimit: {
-    margin: GlobalStyles.Margin.margin_8xs,
-  },
-  theSpend: {
-    fontSize: GlobalStyles.FontSize.size_3xs,
-  },
-  cardSpendingLimitTheSpend: {
-    marginTop: -15.5,
-    left: 27,
-    lineHeight: 18,
-    width: 257,
-    top: "50%",
-  },
-  meter1Icon: {
-    width: 17,
-    height: 17,
-    top: 0,
-  },
-  cardSpendingLimitTheSpendParent: {
-    top: 20,
-    left: 21,
-    height: 31,
-  },
-  groupItem: {
+  carbonSpendingAnalysysBarProgress: {
     height: "100%",
-    top: "0%",
-    right: "0%",
-    bottom: "0%",
-    left: "0%",
-    borderRadius: GlobalStyles.Border.br_sm,
-    backgroundColor: GlobalStyles.Color.gray_600,
-    position: "absolute",
-    width: "100%",
+    backgroundColor: "orange",
+    borderRadius: 15,
+    maxWidth: "100%",
   },
-  groupInner: {
-    height: "76.92%",
-    width: "43.48%",
-    top: "11.54%",
-    right: "50%",
-    bottom: "11.54%",
-    left: "6.52%",
-    maxHeight: "100%",
-  },
-  rectangleGroup: {
-    top: 23,
-    width: 46,
-    height: 26,
-  },
-  groupIcon: {
-    marginTop: -30.03,
-    right: "40%",
-    left: "50%",
-    marginLeft: -97,
-    height: 150,
-    width: 290,
-    top: "50%",
-  },
-  rectangleParent: {
-    top: 138,
-    bottom: 0,
-  },
-  hello1: {
-    marginTop: 26,
-    marginLeft: -38,
-    fontSize: GlobalStyles.FontSize.size_7xl,
-    top: "50%",
-    fontWeight: "700",
-  },
-  hello2: {
-    marginTop: 59,
-    marginLeft: -30,
-    top: "50%",
-    fontWeight: "700",
-  },
-  hello3: {
-    marginLeft: -35,
-    bottom: 16,
-    fontSize: GlobalStyles.FontSize.size_3xs,
-  },
-  theLimitDeterminesTheAmoun: {
-    top: 40,
-    left: 3,
-    lineHeight: 16,
-    width: 316,
-  },
-  cardIcon: {
-    marginLeft: -6,
-    bottom: 38,
-    width: 12,
-    height: 9,
-    left: "50%",
-    position: "absolute",
-  },
-  helloParent: {
-    width: "100%",
-    height: 500,
-  },
-  arrowPosition: {
-    top: "6%",
-    position: "absolute",
-  },
-  arrowCircle: {
-    marginTop: -10.11,
-    marginRight: 303.54,
-    width: 15,
-    height: 15,
-  },
-  spendingLimit: {
-    flex: 1,
-    paddingLeft: GlobalStyles.Padding.padding_7xs,
-    paddingRight: GlobalStyles.Padding.padding_8xs,
-    width: "100%",
+  barText: {
+    left: 20,
+    top: "25%",
+    fontSize: 15,
+    fontWeight: "bold",
   },
 });
 
