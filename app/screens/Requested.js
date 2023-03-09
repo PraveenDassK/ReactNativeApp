a697eccb9e0491c425ce6948b91125a86b7f7ae6
Jack Huang <jack.h@carbonyte.io>
2023-03-09 10:15:52 +0000

    Updated Requested.js

--- app/screens/Requested.js
+++ app/screens/Requested.js
@@ -1,102 +1,96 @@
-import * as React from "react";
-import { StyleSheet, View, Text, Image, Pressable } from "react-native";
-
+import React, { useContext, useEffect, useState } from "react"; 
+//import * as React from "react";
+import { StyleSheet, View, SafeAreaView, Text, TextInput, Image,Pressable, useWindowDimensions, Dimensions, TouchableWithoutFeedback, Keyboard} from "react-native";
+import Screen from "../components/Screen";
+import AuthContext from "../auth/context";
 import GlobalStyles from "../../GlobalStyles";
+import api from "../api/api_list";
+import { Formik } from "formik";
+import * as Yup from 'yup';
+import { horizontalScale, verticalScale, moderateScale } from "../config/scaling"
+import ErrorMessage from "../components/forms/ErrorMessage";
+import Button from "../components/Button"
 
-const Requested = ({route,navigation}) => {
-  console.log(route.params.amount)
-  let amount = route.params.amount
-  let name = route.params.name
-  return (
-    <Pressable
-      style={styles.requested}
-      onPress={() => navigation.navigate("AccountMain")}
-    >
-      <View style={styles.groupParent}>
-        <View style={styles.groupPosition}>
-          <View style={[styles.groupChild, styles.groupPosition]} />
-        </View>
-        <Text style={[styles.hello, styles.helloTypo]}>
-          <Text style={styles.youHaveRequested}>You have requested £{amount}{'\n'}</Text>
-          <Text style={styles.youHaveRequested}>from {name}</Text>
-        </Text>
-        <Image
-          style={[styles.iconAwesomeCheckCircle, styles.hello1Position]}
-          resizeMode="cover"
-          source={require("../assets/icon-awesomecheckcircle.png")}
-        />
-        <Text style={[styles.hello1, styles.hello1Position, styles.helloTypo]}>
-          Tap anywhere to continue
-        </Text>
-      </View>
-    </Pressable>
+import DatePicker from 'react-native-date-picker'
+import RegistrationPersonalDetails from "../components/RegistrationPersonalDetails.jsx"
+import RegistraionEmailAndPhone from "../components/RegistrationPersonalDetails.jsx"
+import PersonalOrBusiness from "../components/RegistrationPersonalOrBusiness.jsx"
+
+const Registration = ({navigation}) => {
+
+      //Personal details
+      const [accountType, setPersonalBusiness] = useState(null);
+      const [personalDetails, setPersonalDetails] = useState(null)
+      const [emailandPhone, setEmailandPhone] = useState(null)
+      const [nationality, setNationality] = useState(null)
+      const [addresses, setAddresses] = useState(null)
+  
+      const [registrationNumberDetails, setRegistrationNumberDetails] = useState(null)
+      const [companyDetails, setCompanyDetails] = useState(null)
+      const [screenToShow, setScreenToShow] = useState(null)
+  
+      /**
+       * @dev Use this to differentiate between Personal or business accounts
+       * @param {String} type This is the choice of which account to use
+       */
+      const accountSelector = async(type) => {
+        await sendDetails()
+  
+        if(type == "Personal"){
+          setScreenToShow("Registration")
+          setPersonalBusiness(type)
+        }else{
+          setScreenToShow("BusinessRegNumber")
+          setPersonalBusiness(type)
+        }
+        console.log(type)
+      }
+  
+      /**
+       * @notice Validation for the objects are done on the Component side
+       * @todo Add business details in
+       * @param {Object} details The details that the component sends back
+       * @param {Str} page This is the page that the information is coming from
+       */
+      const detailsSaver = (details,page) => {
+        switch(page){
+          case "RegistrationPersonalDetails":
+            setPersonalDetails(details)
+            setScreenToShow("RegistrationEmailAndPhone")
+            break;
+        case "RegistrationEmailAndPhone":
+            setPersonalDetails(details)
+            setScreenToShow("EmailandPhone")
+            break;
+        case "PersonalOrBusiness":
+            break;
+        }
+      }
+  
+      const pagePicker = () => {
+        switch(screenToShow){
+          case "RegistrationPersonalDetails":
+            return <RegistrationPersonalDetails SaveDetails = {detailsSaver}/>
+        case "RegistrationEmailAndPhone":
+            return <RegistrationPersonalDetails SaveDetails = {detailsSaver}/>
+        default:
+            return <PersonalOrBusiness chooseAccount = {accountSelector}/>
+        }
+      }
+
+    const handleSubmit = async ({ email, phoneNumber }) => {
+        
+    }
+
+
+    return (
+        <Screen>
+            {pagePicker()}
+        </Screen>
   );
 };
 
 const styles = StyleSheet.create({
-  groupPosition: {
-    left: 0,
-    bottom: 0,
-    right: 0,
-    top: 0,
-    position: "absolute",
-  },
-  helloTypo: {
-    textAlign: "center",
-  },
-  hello1Position: {
-    top: "50%",
-    position: "absolute",
-  },
-  groupChild: {
-    borderTopLeftRadius: GlobalStyles.Border.br_4xl,
-    borderTopRightRadius: GlobalStyles.Border.br_4xl,
-    shadowColor: "rgba(1, 1, 253, 0.1)",
-    shadowOffset: {
-      width: 0,
-      height: -3,
-    },
-    shadowRadius: 20,
-    elevation: 20,
-    shadowOpacity: 1,
-    backgroundColor: GlobalStyles.Color.white,
-  },
-  youHaveRequested: {
-    margin: GlobalStyles.Margin.margin_8xs,
-  },
-  hello: {
-    top: 458,
-    fontSize: GlobalStyles.FontSize.size_4xl,
-    lineHeight: 24,
-    fontWeight: "700",
-    color: GlobalStyles.Color.indigo_100,
-    position: "absolute",
-    textAlign: "center",
-    width:"100%"
-  },
-  iconAwesomeCheckCircle: {
-    marginTop: -138.06,
-    marginLeft: -93.56,
-    left: "50%",
-    width: 187,
-    height: 187,
-  },
-  hello1: {
-    marginTop: 332.5,
-    left: "50%",
-    marginLeft:-60,
-    fontSize: GlobalStyles.FontSize.size_xs,
-    color: GlobalStyles.Color.gray_700,
-  },
-  groupParent: {
-    width: "100%",
-    height: 749,
-  },
-  requested: {
-    flex: 1,
-    width: "100%",
-    backgroundColor: GlobalStyles.Color.white,
-  },
 });
 
-export default Requested;
+export default Registration;
