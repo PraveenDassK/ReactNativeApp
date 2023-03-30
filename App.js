84539d204c448d834926456b8949a4022d4f07d4
chisambwe.k <chisambwe.k@carbonyte.io>
2023-03-30 10:41:47 +0100

    Updated App.js

--- App.js
+++ App.js
@@ -252,7 +252,7 @@ async function registerForPushNotificationsAsync() {
   } else {
     alert('Must use physical device for Push Notifications');
   }
-  console.log("token", token)
+  console.log("pushToken", token)
 
   return token;
 }
