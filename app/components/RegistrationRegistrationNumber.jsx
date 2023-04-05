f161d25d0cca67013be1e044f08c13395ae07eb1
chisambwe.k <chisambwe.k@carbonyte.io>
2023-04-05 10:38:25 +0100

    Updated Registration Validation

--- app/components/RegistrationRegistrationNumber.jsx
+++ app/components/RegistrationRegistrationNumber.jsx
@@ -1,68 +1,106 @@
-import React, { useContext, useEffect, useState } from "react"; 
+import React, { useContext, useEffect, useState } from "react";
 //import * as React from "react";
-import { StyleSheet, View, SafeAreaView, Text, TextInput, Image,Pressable, useWindowDimensions, Dimensions, TouchableWithoutFeedback, Keyboard, Alert} from "react-native";
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Text,
+  TextInput,
+  Image,
+  Pressable,
+  useWindowDimensions,
+  Dimensions,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+} from "react-native";
 import Screen from "./Screen";
 import AuthContext from "../auth/context";
 import GlobalStyles from "../../GlobalStyles";
 import apiLoginRegister from "../api/apiLogin";
-import * as Yup from 'yup';
-import { horizontalScale, verticalScale, moderateScale } from "../config/scaling"
+import * as Yup from "yup";
+import {
+  horizontalScale,
+  verticalScale,
+  moderateScale,
+} from "../config/scaling";
 
-import Button from "./AppButton"
-import { Formik, Field, Form } from 'formik';
-import { Dropdown } from 'react-native-element-dropdown';
-import { CheckBox } from '@rneui/themed';
+import Button from "./AppButton";
+import { Formik, Field, Form } from "formik";
+import { Dropdown } from "react-native-element-dropdown";
+import { CheckBox } from "@rneui/themed";
 import AuthScreen from "./AuthScreen";
 
+const PersonalDetails = ({ SaveDetails, setScreenToShow }) => {
+  const [regNum, setRegNum] = useState("");
+  const [companyDetails, setCompanyDetails] = useState(null);
 
-const PersonalDetails = ({SaveDetails,setScreenToShow}) => {
-    const [regNum, setRegNum] = useState("");
-    const [companyDetails, setCompanyDetails] = useState(null)
+  const searchCompany = async () => {
+    if (regNum == "") return Alert.alert("Invalid company address");
+    const request = await apiLoginRegister.GetCompanyByRegNo(regNum);
+    if (request === null) return Alert.alert("Invalid company address");
+    setCompanyDetails(request);
+    console.log(request);
+  };
 
-    const searchCompany = async() => {
-        if(regNum == "") return Alert.alert('Invalid company address')
-        const request = await apiLoginRegister.GetCompanyByRegNo(regNum)
-        if(request === null) return Alert.alert('Invalid company address')
-        setCompanyDetails(request)
-        console.log(request)
-    }
+  const sendDetails = () => {
+    SaveDetails(companyDetails, "RegistrationNumber");
+  };
+  console.log(regNum);
 
-    const sendDetails = () => {
-        SaveDetails(companyDetails,"RegistrationNumber")
-    }
-    console.log(regNum)
-
-    const handleBack = () =>{
-        console.log("!")
-        setScreenToShow("")
-      }
-    return (
-        <Screen style={{backgroundColor: "white"}}>
-            <AuthScreen title="Company registration number" img="elephantCarbon" handleBack = {handleBack}>
-            <View style={[styles.component1981, styles.mt14]}>
-                    <TextInput 
-                        placeholder="Company registration number"
-                        placeholderTextColor="grey"
-                        keyboardType="numeric" 
-                        onChangeText={(value) => setRegNum(value)}
-                        style={[styles.component1981Child, styles.childBorder, {padding:10}]} 
-                    />
-                </View>
-                {companyDetails?
-                <View style={styles.spacing}>
-                    <Text style={styles.spacing}>Are theese your details?</Text>    
-                    <Text>{companyDetails.registered_office_address.address_line_1}</Text>    
-                    <Text>{companyDetails.registered_office_address.address_line_2}</Text>    
-                    <Text>{companyDetails.registered_office_address.locality}</Text>    
-                    <Text>{companyDetails.registered_office_address.postal_code}</Text>    
-                </View>
-                : null}
-            <Button title="Search" color="white" textColor="black" onPress={() => searchCompany()} />
-            {companyDetails ?  
-                <Button title="Continue" color="black" textColor="white" onPress={() => sendDetails()} />
-            : null}
-            </AuthScreen>
-            {/* <Text>Company registration number</Text>
+  const handleBack = () => {
+    console.log("!");
+    setScreenToShow("");
+  };
+  return (
+    <Screen style={{ backgroundColor: "white" }}>
+      <AuthScreen
+        title="Company registration number"
+        img="elephantCarbon"
+        handleBack={handleBack}
+      >
+        <View style={[styles.component1981, styles.mt14]}>
+          <TextInput
+            placeholder="Company registration number"
+            placeholderTextColor="grey"
+            keyboardType="numeric"
+            onChangeText={(value) => setRegNum(value)}
+            style={[
+              styles.component1981Child,
+              styles.childBorder,
+              { padding: 10 },
+            ]}
+          />
+        </View>
+        {companyDetails ? (
+          <View style={styles.spacing}>
+            <Text style={styles.spacing}>Are these your details?</Text>
+            <Text>
+              {companyDetails.registered_office_address.address_line_1}
+            </Text>
+            <Text>
+              {companyDetails.registered_office_address.address_line_2}
+            </Text>
+            <Text>{companyDetails.registered_office_address.locality}</Text>
+            <Text>{companyDetails.registered_office_address.postal_code}</Text>
+          </View>
+        ) : null}
+        <Button
+          title="Search"
+          color="white"
+          textColor="black"
+          onPress={() => searchCompany()}
+        />
+        {companyDetails ? (
+          <Button
+            title="Continue"
+            color="black"
+            textColor="white"
+            onPress={() => sendDetails()}
+          />
+        ) : null}
+      </AuthScreen>
+      {/* <Text>Company registration number</Text>
                 <View style={[styles.component1981, styles.mt14,{marginLeft:horizontalScale(10)}]}>
                     <TextInput 
                         keyboardType="numeric" 
@@ -83,24 +121,23 @@ const PersonalDetails = ({SaveDetails,setScreenToShow}) => {
             {companyDetails ?  
                 <Button title="Continue" color="babyBlue" onPress={() => sendDetails()} />
             : null} */}
-        </Screen>
+    </Screen>
   );
 };
 
 const styles = StyleSheet.create({
-    childBorder: {
-        borderWidth: 0.5,
-        borderRadius: 10,
-        borderColor: "#D3D3D3",
-        opacity: 1,
-        height: 50,
-        marginTop: "2.5%",
-        marginBottom: "5%",
-        
-      }, 
-      spacing: {
-        marginBottom: "2.5%"
-      }
+  childBorder: {
+    borderWidth: 0.5,
+    borderRadius: 10,
+    borderColor: "#D3D3D3",
+    opacity: 1,
+    height: 50,
+    marginTop: "2.5%",
+    marginBottom: "5%",
+  },
+  spacing: {
+    marginBottom: "2.5%",
+  },
 });
 
 export default PersonalDetails;
