064a824437862ddc770b7e9573c90485e0b6be6b
Jack Huang <jack.h@carbonyte.io>
2023-04-05 13:57:50 +0000

    Updated RegistrationCompanyAddress.jsx

--- app/components/RegistrationCompanyAddress.jsx
+++ app/components/RegistrationCompanyAddress.jsx
@@ -10,42 +10,76 @@ import * as Yup from 'yup';
 import { horizontalScale, verticalScale, moderateScale } from "../config/scaling"
 import Button from "./AppButton"
 import AuthScreen from "./AuthScreen";
+import PostCode from './RegistrationPostCode.jsx'
+
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"),
     phoneNumber: Yup.string().required().min(10).max(10).label("Phone number")
 })
 
-const PastAddresses = ({SaveDetails,setScreenToShow}) => {
-    const handleSubmit = async () => {
-        SaveDetails(null,"CompanyAddress")
-    }
-    const handleBack = () =>{
-        console.log("!")
-        setScreenToShow("CompanyDetails")
-      }
-    return (
-        <Screen>
-            <AuthScreen title="Company address" img="elephantCarbon" handleBack = {handleBack}>
-            <Formik
-            initialValues={{
-                email:'', 
-                phoneNumber: ''
-            }}
-            onSubmit={handleSubmit}
-            validationSchema={validationSchema}
-            >
-            {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
-                <View style={[styles.component1981, styles.mt14]}>
-                    <Text>Past addresses</Text>
-                    <Button title="Add" color="white" textColor="black" onPress={() => handleSubmit()} />
+const CompanyAddress = ({SaveDetails,setScreenToShow}) => {
+    
+    const [addresses, setAddresses] = useState([]);
+    const [isAdding, setAdding] = useState(false);
+    const [add, setAdd] = useState(null)
 
+    useEffect(()=>{
+        if(addresses.length > 0 ) setAdd(addresses.address1)
+    },[addresses, isAdding])
+
+    const handleSubmit = async () => {
+        SaveDetails(addresses,"PastAddresses")
+    }
+
+    /**
+     * @dev This sets the is adding to false which will show the past addresses page when used
+     * @notice This does not validate data, validate the data on entry
+     * @param {Object} address The address object that the address finder passes back
+     */
+    const addAddress = (addressObj) => {
+        setAdding(false)
+        setAddresses(address => [...address, addressObj])
+    }
+    console.log("Addresses", addresses)
+
+    const handleBack = () => {
+        console.log("!");
+        setScreenToShow("CompanyDetails");
+      };
+    return (
+        
+        <Screen>
+            <AuthScreen title="Where is it located?" img="elephantCard" width="70%"
+            handleBack={handleBack}
+            >
+                
+               {addresses.length > 0  ? (
+                <>
+                    <Text>{`${addresses[0].address1}`}</Text> 
+                    <Text>{`${addresses[0].address2} `}</Text> 
+                    <Text>{`${addresses[0].area} `}</Text> 
+                    <Text>{`${addresses[0].postcode} `}</Text> 
+                </>
+               ): null}
+               {console.log(addresses)}
+            {isAdding ? 
+                <PostCode AddAddress = {addAddress}/>
+          :
+                <View>
+                    <Button title="Add" textColor="white" color="black" onPress={() => setAdding(true)} />
+                    <Button title="Continue" textColor="black" color="white" onPress={() => handleSubmit()} />
                 </View>
-                )}
-            </Formik>
-            <Button title="Continue" color="black" textColor="white" onPress={() => handleSubmit()} />
+            }
             </AuthScreen>
-           
-   
+            {/* <Text>Past addresses</Text>
+            {isAdding ? 
+                <PostCode AddAddress = {addAddress}/>
+          :
+                <View>
+                    <Button title="Add" color="babyBlue" onPress={() => setAdding(true)} />
+                    <Button title="Continue" color="babyBlue" onPress={() => handleSubmit()} />
+                </View>
+            } */}
         </Screen>
   );
 };
@@ -53,4 +87,4 @@ const PastAddresses = ({SaveDetails,setScreenToShow}) => {
 const styles = StyleSheet.create({
 });
 
-export default PastAddresses;
+export default CompanyAddress;
