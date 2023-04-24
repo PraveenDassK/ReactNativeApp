2f6d02ca6eb88575e23e810e8816574c65b7dd7a
chisambwe.k <chisambwe.k@carbonyte.io>
2023-04-24 16:49:26 +0100

    Added DeleteAccount

--- app/screens/DeleteAccount.js
+++ app/screens/DeleteAccount.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Button from "../components/AppButton";
 
 const DeleteAccount = () => {
   return (
-    <div>DeleteAccount</div>
-  )
-}
+    <View style={styles.container}>
+      <Text>Do you want to delete your account?</Text>
+      <Button
+        title="yes"
+        textColor="black"
+        color="white"
+        onPress={() => console.log("yes pressed")}
+      />
+      <Button
+        title="no"
+        textColor="white"
+        color="black"
+        onPress={() => console.log("no pressed")}
+      />
+    </View>
+  );
+};
 
-export default DeleteAccount
+export default DeleteAccount;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center", padding: "10%" },
+});
