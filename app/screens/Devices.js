504fe8996980b65585243a62febc749229706d15
Jack Huang <jack.h@carbonyte.io>
2023-04-25 08:43:15 +0000

    Added Devices.js to /app/screens

---
+++ app/screens/Devices.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState, useContext } from "react";
+import { FlatList, Text, StyleSheet, Image, View, Pressable, ScrollView } from "react-native";
+import GlobalStyles from "../../GlobalStyles";
+import Button from "../components/Button";
+
+import apiDevices from "../api/apiDevices"
+import AuthContext from "../auth/context";
+
+
+const TestEnviro = ({ navigation }) => {
+  const { customerDetails } = useContext(AuthContext)
+  const [devices, setDevices] = useState([])
+
+  useEffect(() => {
+    loadData()
+  }, [customerDetails])
+
+  /**
+   * @dev Loads the device data into devices
+   * @returns Null if no customer details are present
+   *          The return should only be for devlopment only
+   */
+  const loadData = async () => {
+    console.log("load")
+    if (!customerDetails) return;
+    const request = await apiDevices.GetDevices(customerDetails)
+
+    //For each device add it to the hook
+    request.details.forEach((device, i) => {
+      const newDevice = {
+        id: i,
+        device: device
+      }
+      setDevices(prevDevices => [...prevDevices, newDevice]);
+    });
+  }
+
+  return (
+    <View>
+      <Button
+        title="Reload"
+        color="babyBlue"
+        style={styles.boxShadow}
+        onPress={() => loadData()}
+      />
+
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  page: {
+
+  },
+
+});
+
+export default TestEnviro;
