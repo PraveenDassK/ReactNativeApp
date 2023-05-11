e9e1a284712cb7585fcab0243133bea563614082
chisambwe.k <chisambwe.k@carbonyte.io>
2023-05-11 16:40:21 +0100

    Refactored apiDevices

--- app/api/apiDevices.js
+++ app/api/apiDevices.js
@@ -1,5 +1,4 @@
 import client from "./client";
-import jwt_decode from "jwt-decode";
 
 const GetDevices = async (customerID) => {
   const request = await client.get(
@@ -10,28 +9,16 @@ const GetDevices = async (customerID) => {
 
 const DeleteDevice = async (customerId, deviceId) => {
   const deleteDeviceEndpoint = `/authverifymodule/DeleteUserDevice/${customerId}/${deviceId}`;
-  console.log(typeof deviceId, typeof customerId);
-
   try {
-    const response = await client.delete(deleteDeviceEndpoint, {
-      customerId,
-      deviceId,
-    });
-
-    console.log("Request:", response.config);
-    console.log("Response:", response);
+    const response = await client.delete(deleteDeviceEndpoint);
 
     if (response.ok) {
-      // The response is ok, so we can use the data
-      console.log("Data:", response.data);
       return response.ok;
     } else {
-      // The response is not ok, so we handle the error
-      console.log("Error1:", response.data);
       return response.ok;
     }
   } catch (error) {
-    console.log("Error2:", error);
+    console.log("Error:", error);
   }
 };
 
