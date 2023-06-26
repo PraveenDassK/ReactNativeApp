b69e6b678a2feb336516834717a0290c61cd435e
Jack <jack.h@carbonyte.io>
2023-06-26 17:41:16 +0100

    Finished CountdownBar

--- app/components/CountdownBar.jsx
+++ app/components/CountdownBar.jsx
@@ -54,7 +54,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
-        width: '60%',
+        width: '80%',
     },
 });
 
