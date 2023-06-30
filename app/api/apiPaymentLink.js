c773e3cded67e1640025acbb370882ccc1a19649
Jack <jack.h@carbonyte.io>
2023-06-30 16:59:40 +0100

    Added payment link api

---
+++ app/api/apiPaymentLink.js
@@ -0,0 +1,38 @@
+import client from "./client";
+
+const getPaymentLink = async () => {
+  const obj = {
+    "paymentAmount": {
+      "currency": "GBP",
+      "value": 10
+    },
+    "paymentReference": "Payment from Jack",
+    "destination": {
+      "type": "ACCOUNT",
+      "id": "A122HTHM",
+      "accountNumber": "03011666",
+      "sortCode": "000000",
+      "name": "Jack"
+    },
+    "aspspId": "H120000001",
+    "paymentContext": {
+      "paymentContextCode": "",
+      "deliveryAddress": {
+        "addressLine1": "",
+        "addressLine2": "",
+        "postTown": "",
+        "postCode": "",
+        "country": ""
+      },
+      "merchantCategoryCode": "",
+      "merchantCustomerIdentification": ""
+    }
+  }
+  const url = `https://api.carbonyte.io/walletmodule/InitiatePaymentFromASPSP`
+  const PaymentUrlRequest = await client.post(url, obj)
+  return PaymentUrlRequest.data.details.redirectUrl
+}
+
+export default {
+  getPaymentLink,
+}
