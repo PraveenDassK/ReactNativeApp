36012f47b1137ac75dc842376698a8e7a5d54b91
chisambwe.k <chisambwe.k@carbonyte.io>
2023-07-12 10:28:12 +0100

    Update GroupSendMoney spacing

--- app/screens/GroupSendMoney.js
+++ app/screens/GroupSendMoney.js
@@ -557,7 +557,7 @@ const styles = StyleSheet.create({
   },
   flatListContentContainer: {
     
-    paddingHorizontal: "5%",flex: 1
+    padding: "5%",flex: 1
   },
   itemContainer: {
     paddingHorizontal: 22,
