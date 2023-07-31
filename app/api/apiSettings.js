3b8b58370c1c2b81c3cb77dc18e81727c5372b11
chisambwe.k <chisambwe.k@carbonyte.io>
2023-07-31 14:43:24 +0100

    Update settings api call, Update Keyboard avoider for platform specific markup

--- app/api/apiSettings.js
+++ app/api/apiSettings.js
@@ -1,40 +1,41 @@
 // import client from "./client";
-import { jwtClient as client } from './client'
-
+import { jwtClient as client } from "./client";
+import { format as prettyFormat } from "pretty-format";
 /**
- * @dev This gets the card settings 
+ * @dev This gets the card settings
  * @param {Account ID} Id
  * @returns an object of the toggles
  */
 const GetSettings = async (Id) => {
-    try {
-        const request = await client.get(
-            `https://api.carbonyte.io/cardmodule/GetToggles?modulrAccountId=${Id}`
-        );
-        const requestData = request?.data?.details;
-        return requestData;
-    } catch {
-        SetToggles(Id,false,false,false,false)
-        return(GetSettings(Id))
-    }
+  try {
+    const response = await client.get(
+      `/cardmodule/GetToggles?cardId=${Id}`
+    );
+    const requestData = response?.data?.details;
+    console.log(("getsettings", prettyFormat(response)));
+    return requestData;
+  } catch {
+    SetToggles(Id, false, false, false, false);
+    return GetSettings(Id);
+  }
 };
 
-const SetToggles = (enfuceid,
-    online,
-    swipe,
-    atm,
-    contactless
-) => {
-    const request = "https://api.carbonyte.io/cardmodule/SetToggles?accountId=" +
-        enfuceid + "&onlineTransactions=" +
-        online + "&swipePayments=" +
-        swipe + "&atmWithdrawals=" +
-        atm + "&contactlessPayments=" +
-        contactless
-    return client.post(request)
-}
+const SetToggles = (enfuceid, online, swipe, atm, contactless) => {
+  const request =
+    "/cardmodule/SetToggles?cardId=" +
+    enfuceid +
+    "&onlineTransactions=" +
+    online +
+    "&swipePayments=" +
+    swipe +
+    "&atmWithdrawals=" +
+    atm +
+    "&contactlessPayments=" +
+    contactless;
+  return client.post(request);
+};
 
 export default {
-    GetSettings,
-    SetToggles
-}
+  GetSettings,
+  SetToggles,
+};
